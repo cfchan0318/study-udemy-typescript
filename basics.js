5e1dbf43c3bf27ec5c16d4f79cb5d5eda7e214ca
cfchan0318 <cf.chan0318@gmail.com>
2026-08-05 09:53:09 +0800

    Add initial project files including .gitignore, basics.js, basics.ts, and package.json

---
+++ basics.js
@@ -0,0 +1,4 @@
+//Math.round("Hello")
+var pi = 3.14159;
+console.log("Hello World");
+console.log(Math.round(7.3123123123));
---
+++ basics.ts
@@ -0,0 +1,5 @@
+//Math.round("Hello")
+let pi: number = 3.14159;
+console.log("Hello World")
+console.log(Math.round(7.3123123123));
+
