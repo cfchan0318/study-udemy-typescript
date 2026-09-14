3be484812d0fe1a0604577999a4a7862d4bb0db0
cfchan0318 <cf.chan0318@gmail.com>
2026-09-15 00:02:49 +0800

    exercise/section 6 - arrays - learn about array types, 2d arrays and Type<T> annotation for arrays for custom types

---
+++ section-6-arrays/arrays.ts
@@ -0,0 +1,26 @@
+//doesnt work
+//const activeUsers: [] = [];
+const activeUsers: string[] = ["curt"];
+
+activeUsers.push("steven")
+
+let users: string[] = []
+users.push("tony")
+
+let ageList: number[] = []
+ageList[0] = 99
+
+
+type Point = {
+    x: number,
+    y: number
+}
+
+let pointArr: Array<Point> = []
+pointArr.push({ x: 1, y: 2 }) 
+
+//2d array
+const board: string[][] = [
+    ["X", "O", "X"], ["X", "O", "X"], ["X", "O", "X"]]
+
+const demo:number[][][] = [[[1]]]
